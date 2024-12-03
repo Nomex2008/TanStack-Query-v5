6c269659c914eb91df0f4f01867f780580051deb
Nomex2008 <97978327+Nomex2008@users.noreply.github.com>
2024-12-03 19:38:44 +0200

    Commit 1(useQuery)
    Add useQuery

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
+import { useQuery } from '@tanstack/react-query'
 import './App.css'
 
+const getData = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+
+  return response.json()
+}
+
 function App() {
+  const {data, error, isLoading} = useQuery({
+    queryKey: ['posts'], 
+    queryFn: getData
+  })
+
+  console.log(error)
+
   return (
     <>
-      React Query
+      <h1>React Query</h1>
+
+      <div>
+        {isLoading
+        ? 'Loading...'
+        : data?.length
+        ? data.map((post: any) => (
+          <div key = {post.id}>{post.title}</div>
+        )) 
+        : 'Not found' }
+      </div>
     </>
   )
 }
