e92516a386f82570db10d383644006caef30f355
Nomex2008 <97978327+Nomex2008@users.noreply.github.com>
2024-12-04 23:45:06 +0200

    Commit 2(Axios)
    Add axios.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
 import './App.css'
 
 const getData = async () => {
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts')
-
-  return response.json()
+  return axios.get('https://jsonplaceholder.typicode.com/posts')
 }
 
 function App() {
   const {data, error, isLoading} = useQuery({
     queryKey: ['posts'], 
-    queryFn: getData
+    queryFn: getData,
+    select: data => data.data
   })
 
   console.log(error)
